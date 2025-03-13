7f6f732b9d2703d3acfa42f57076819f8164e2fb
vcdice-69 <160406447+vcdice-69@users.noreply.github.com>
2025-03-13 23:08:44 +0800

    Add files via upload

---
+++ CarparkFinder/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import GoogleMapComponent from './GoogleMap';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Carpark Finder</h1>
+      <GoogleMapComponent />
+    </div>
+  );
+}
+
+export default App;
